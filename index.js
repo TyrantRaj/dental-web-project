6eb11b4c5e129f3c521fad64c480503e79120cc0
TyrantRaj <tyrantraj@gmail.com>
2024-04-07 16:14:01 +0530

    about us compleated

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-var next_btn = $(".imghoverright");
-var pre_btn = $(".imghoverleft");
+var next_btn_img = $(".imghoverright");
+var pre_btn_img = $(".imghoverleft");
+
+var next_btn = $(".carousel-control-next-icon")
+var pre_btn = $(".carousel-control-prev-icon")
 
 next_btn.on("mouseenter",mouseonr).on("mouseleave",mouseoffr);
 
 function mouseonr(){
-    next_btn.css("background-image","url(images/caret-right-fill.svg)")
+    next_btn_img.css("background-image","url(images/caret-right-fill.svg)")
 }
 
 function mouseoffr(){
-    next_btn.css("background-image","url(images/caret-right.svg.svg)")
+    next_btn_img.css("background-image","url(images/caret-right.svg.svg)")
 }
 
 
 pre_btn.on("mouseenter",mouseonl).on("mouseleave",mouseoffl);
 
 function mouseonl(){
-    pre_btn.css("background-image","url(images/caret-left-fill.svg)")
+    pre_btn_img.css("background-image","url(images/caret-left-fill.svg)")
 }
 
 function mouseoffl(){
-    pre_btn.css("background-image","url(images/caret-left.svg.svg)")
+    pre_btn_img.css("background-image","url(images/caret-left.svg.svg)")
 }
+
+function swichslide() {
+    next_btn_img.element.click();
+ }
+
+ setInterval(swichslide, 5000);
