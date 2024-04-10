f861301a528f05383aaad9b04ff351ba12f24dc1
TyrantRaj <tyrantraj@gmail.com>
2024-04-10 13:06:18 +0530

    sending mail done

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var pre_btn_img = $(".imghoverleft");
 
 var next_btn = $(".carousel-control-next-icon")
 var pre_btn = $(".carousel-control-prev-icon")
+const btn = $(".submit_btn")
+
 
 next_btn.on("mouseenter",mouseonr).on("mouseleave",mouseoffr);
 
@@ -25,8 +27,43 @@ function mouseoffl(){
     pre_btn_img.css("background-image","url(images/caret-left.svg)")
 }
 
-// function swichslide() {
-//     next.element.click();
-//  }
 
-//  setInterval(swichslide, 5000);
+var onSuccess = function(response) {
+    alert("Success!");
+    console.log(response);
+  };
+  
+var onError = function(err) {
+    alert("Error!");
+    console.error(err);
+  };
+  
+function sendEmail(endpointUrl, data, onSuccess, onError) {
+    $.ajax({
+        type: "POST",
+        url: endpointUrl,
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: onSuccess,
+        // error: function(xhr, status) {
+        //   if (typeof this.statusCode[xhr.status] !== 'undefined') {
+        //     return false;
+        //   }
+  
+        //   onError(err);
+        // },
+        statusCode: {
+          // Endpoint thinks that it's likely a spam/bot request, you need to change "spam protection mode" to "never" in HeroTofu forms
+          422: function(response) {
+            alert("Cannot send request, are you robot?");
+          },
+        }
+      });
+}
+
+
+btn.click( sendEmail("https://herotofu.com/start", {
+    example_user: "user@example.com",
+    example_data: new Date().toISOString(),
+  }, onSuccess, onError));
